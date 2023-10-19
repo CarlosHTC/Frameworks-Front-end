914df4e17094ff516bfa448069edd5e73a9cc11f
Carlos <chtcarneiro@gmail.com>
2023-10-18 20:47:49 -0400

    Projeto Dito Lambari

---
+++ Projeto Dito Lambari/index.js
@@ -0,0 +1,35 @@
+let localDate = getNewDate();
+
+// localDate = "POCONÉ," + toString(getNewDate().ano) + "DE" + toString(getNewDate().mes) + "DE" + toString(getNewDate().ano)
+document.getElementById("localData").innerHTML = localDate
+
+function getNewDate() {
+    let meses = [
+        {name: "JANEIRO"},
+        {name: "FEVEREIRO"},
+        {name: "MARÇO"},
+        {name: "ABRIL"},
+        {name: "MAIO"},
+        {name: "JUNHO"},
+        {name: "JULHO"},
+        {name: "AGOSTO"},
+        {name: "SETEMBRO"},
+        {name: "OUTUBRO"},
+        {name: "NOVEMBRO"},
+        {name: "DEZEMBRO"},
+    ];
+
+    ano = new Date().getFullYear();
+    mes = new Date().getMonth();
+    dia = new Date().getDay();
+    // x = new Date()
+    if (13 > mes > 0) {
+        mes = meses[mes];
+        dia = dia + 15;
+    } else {
+        mes = "indefinido";
+    }
+
+    return "Cuiabá, " + dia + " DE " + mes.name + " DE " + ano;
+    // return x
+}
